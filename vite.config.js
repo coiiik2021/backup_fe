3bfcb8c16a293f238c1b3c93f90ba08b147d997f
dungcoiii <dungcnttk2021@gmail.com>
2025-06-25 11:07:56 +0700

    fix host

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+
+  server: {
+    host: '0.0.0.0', // Cho phép lắng nghe trên tất cả giao diện
+    port: 5173,
+    allowedHosts: ['conkin.vn', 'www.conkin.vn', '171.244.61.141']
+  },
   plugins: [react()
     , tailwindcss()],
 
